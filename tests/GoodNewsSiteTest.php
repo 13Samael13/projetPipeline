f62783d68c192faf299122dbd026cbfa4602c16d
13Samael13 <roman.allion@student.vinci.be>
2021-12-22 00:25:42 +0100

    test no site build if test error ?

--- tests/GoodNewsSiteTest.php
+++ tests/GoodNewsSiteTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class GoodNewsSiteTest extends TestCase
 {
     public function testStupid(){
-        $this->assertEquals("Yes","Yes");
+        $this->assertEquals("Yes","No");
     }
 
 }
